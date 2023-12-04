b77006b207c4c3319d6ff6bcaca56ff5568764c9
垓恪 Gaiger i5-4670 <drsteingaiger@gmail.com>
2023-12-04 16:21:20 +0800

    - Make printf workable in the console.

--- Qt/main.cpp
+++ Qt/main.cpp
@@ -1,3 +1,11 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <QtGlobal>
+#if defined( Q_OS_WIN )
+#include <windows.h>
+#endif
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -62,6 +70,14 @@ const struct track trackdata = {
 #endif
 int main(int argc, char *argv[])
 {
+#if defined( Q_OS_WIN )
+	if (AttachConsole(ATTACH_PARENT_PROCESS) || AllocConsole()){
+		freopen("CONOUT$", "w", stdout);
+		freopen("CONOUT$", "w", stderr);
+		freopen("CONIN$", "r", stdin);
+	}
+	setvbuf(stdout, NULL, _IONBF, 0);
+#endif
 	QApplication a(argc, argv);
 
 	AudioPlayer player(&a);
